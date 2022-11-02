d63059eabd6845291cc10df25084e5c335eee5e1
Pink <75178570+celsorodrigo@users.noreply.github.com>
2022-11-02 16:07:44 -0300

    Atualizada estrutura do site.

--- src/components/ProjectInfo.js
+++ src/components/ProjectInfo.js
@@ -1,8 +1,23 @@
 import React from "react";
+import placeHolder from "../images/placeHolder.png";
 
 function ProjectInfo() {
   return (
     <div className="project-info">
+      <div className="close-btn">
+        <img src={placeHolder} alt="WIP" />
+      </div>
+      <div className="image">
+        <img src={placeHolder} alt="WIP" />
+      </div>
+      <div className="description">
+        <h2>Porject Name</h2>
+        <p>Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+      </div>
+      <div className="link-containers">
+        <a href="#">Projeto</a>
+        <a href="#">Repositório</a>
+      </div>
     </div>
   );
 }
