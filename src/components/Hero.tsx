ea4a816e827a64eb5317d7c850c5379d1d2fe049
Celso Rodrigo <celsorodrigoma@gmail.com>
2023-10-17 10:08:18 -0300

    feat: Melhorado estilização do hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,23 @@
 import heroImage from "../assets/heroImg.svg"
+import Socials from "./Socials"
 
 function Hero() {
   return (
-    <div className="hero flex justify-center items-center w-screen bg-heroBg bg-no-repeat bg-cover pt-24 pb-4 sm:pt-20 sm:h-screen">
+    <div className="hero flex justify-center bg-heroBg bg-no-repeat bg-cover pb-8 md:h-screen md:pt-28 md:pl-12">
       <div
-        className="flex flex-col h-1/2 gap-2"
+        className="flex flex-col h-1/2 gap-4 pt-8"
       >
         <div>
           <p className="text-2xl sm:text-4xl">Olá, mundo.</p>
           <p className="text-2xl sm:text-4xl">Eu sou o</p>
         </div>
-        <h1 className="font-bold text-4xl sm:text-6xl">Celso Rodrigo</h1>
-        <p className="font-medium text-red text-lg sm:text-xl">Desenvolvedor Web Front-End</p>
+        <div>
+          <h1 className="font-bold text-4xl sm:text-6xl text-black">Celso Rodrigo</h1>
+          <p className="font-medium text-blue text-lg sm:text-xl">Desenvolvedor Web Front-End</p>
+        </div>
       </div>
       <img
-        className="hidden w-2/5 max-h-96 sm:block"
+        className="hidden w-2/5 max-h-96 md:block"
         src={heroImage}
         alt="Ilustração do hero."
       />
