abdda5fcedaf3f93ccf21ddf9125321ae6e4cc80
Celso Rodrigo <celsorodrigoma@gmail.com>
2023-10-21 12:12:34 -0300

    feat: Atualizadas informações dos projetos

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -30,14 +30,43 @@ function ProjectDetails({project, setShowProjectDetails}: IProps) {
 
   return (
     <div
-      className="fixed z-20 left-0 top-0 bg-blackTransparency w-screen min-h-screen"
+      className="flex flex-col items-center gap-4 p-8 pt-16 overflow-y-scroll fixed z-20 left-0 top-0 bottom-0 bg-blackTransparency w-screen min-h-screen sm:gap-8 md:pt-8 md:px-40"
     >
       <button
         onClick={closeProjectDetails}
-        className="fixed right-4 top-4 flex items-center justify-center border rounded-md border-white"
+        className="fixed right-5 top-4 flex items-center justify-center border rounded-md border-white bg-black"
       >
         <ReactSVG src={close} className="fill-white w-10 sm:w-16" />
       </button>
+      <h1
+        className="text-white text-3xl font-bold sm:text-5xl"
+      >
+        {project.title}
+      </h1>
+      <img src={project.img} alt={`${project.title}.`} className="h-36 border-red border-4 shadow-card sm:h-64" />
+      <div
+        className="text-white p-6 font-bold"
+      >
+        {project.description.map((paragraph, index) => <p key={`p-${index}`}>{paragraph}</p>)}
+      </div>
+      <div
+        className="flex flex-col gap-8 sm:flex-row sm:gap-20"
+      >
+        <a 
+          href={project.siteLink} 
+          target="_blank" 
+          className="project-btn"
+        >
+          Projeto
+        </a>
+                <a 
+          href={project.repoLink} 
+          target="_blank" 
+          className="project-btn"
+        >
+          Repositório
+        </a>
+      </div>
     </div>
   )
 }
