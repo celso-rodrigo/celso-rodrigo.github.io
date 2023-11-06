c95814c753347b8b5103ca1fe0d29daf2e18b07e
Celso Rodrigo <celsorodrigoma@gmail.com>
2023-11-06 10:18:45 -0300

    feat: Melhorada responsividade do Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,22 +3,22 @@ import Socials from "./Socials"
 
 function Hero() {
   return (
-    <div className="hero flex justify-center bg-heroBg bg-no-repeat bg-cover pb-8 md:min-h-screen md:pt-28 md:pl-12">
+    <div className="hero flex justify-center items-center bg-heroBg bg-no-repeat bg-cover pb-8 xl:gap-20 lg:gap-12 md:p-0 md:min-h-[calc(100vh-80px)] md:pl-12">
       <div
-        className="flex flex-col h-1/2 gap-4 pt-8"
+        className="flex flex-col h-1/2 pt-8 gap-2 xl:pt-0 lg:gap-8"
       >
         <div>
-          <p className="text-2xl sm:text-4xl">Olá, mundo.</p>
-          <p className="text-2xl sm:text-4xl">Eu sou o</p>
+          <p className="text-2xl xl:text-6xl sm:text-4xl">Olá, mundo.</p>
+          <p className="text-2xl xl:text-6xl sm:text-4xl">Eu sou o</p>
         </div>
         <div>
-          <h1 className="font-bold text-4xl sm:text-6xl text-black">Celso Rodrigo</h1>
-          <p className="font-medium text-blue text-lg sm:text-xl">Desenvolvedor Web Front-End</p>
+          <h1 className="font-bold text-4xl xl:text-7xl sm:text-6xl text-black">Celso Rodrigo</h1>
+          <p className="font-medium text-blue text-lg xl:text-2xl sm:text-xl">Desenvolvedor Web Front-End</p>
         </div>
         <Socials />
       </div>
       <img
-        className="hidden w-2/5 max-h-96 md:block"
+        className="hidden h-1/4 w-1/2 md:block"
         src={heroImage}
         alt="Ilustração do hero."
       />
